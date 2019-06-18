7c88483b28c0e846b4629dbbe11f4085bd7e6c44
Hongli Lai <honglilai@gmail.com>
2019-06-18 11:33:37 +0200

    Change color palette from teal-ish to blue-ish, fix navbar dropdown support

--- helpers/navigation_helpers.rb
+++ helpers/navigation_helpers.rb
@@ -1,10 +1,29 @@
 module NavigationHelpers
   def navbar_link(label, id, current_id, target)
-    link = link_to("<span>#{h label}</span>", url_for(target), 'data-nav-section': id)
+    link = link_to(%Q{<span>#{h label}</span>}, url_for(target), 'data-nav-section': id)
     if id == current_id
-      "<li class=\"active\">#{link}</li>"
+      %Q{<li class="active">#{link}</li>}
     else
-      "<li>#{link}</li>"
+      %Q{<li>#{link}</li>}
     end
+  end
+
+  def navbar_dropdown(label, id, current_id)
+    link = link_to(%Q{<span>#{h label} <span class="caret"></span></span>},
+      '#',
+      class: 'dropdown-toggle',
+      role: 'button',
+      'data-toggle': 'dropdown',
+      'aria-haspopup': true,
+      'aria-expanded': false)
+    active_class =
+      if id == current_id
+        'active'
+      else
+        nil
+      end
+    concat %Q{<li class="dropdown #{active_class}">#{link}<ul class="dropdown-menu">}
+    yield
+    concat %Q{</ul></li>}
   end
 end
